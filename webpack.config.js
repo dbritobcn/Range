cbfc0b24e48cfd6b600326ad89b084b4cec249f4
David Brito <david.brito@bitpanda.com>
2023-01-23 13:06:16 +0100

    Added React

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,17 @@ const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: './src/index.tsx',
   output: {
     filename: 'bundle.js',
+    publicPath: '/',
     path: path.resolve(__dirname, 'public')
   },
-  mode: 'development',
+  mode: process.env.NODE_ENV || 'development',
   module: {
     rules: [
       {
-        test: /\.ts?$/,
+        test: /\.(ts|tsx)$/,
         use: 'ts-loader',
         exclude: /node_modules/,
       },
@@ -24,6 +25,14 @@ module.exports = {
     })
   ],
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js', 'json'],
+    modules: [
+      path.resolve(__dirname, 'src'),
+      'node_modules'
+    ]
   },
+  devServer: {
+    port: 8080,
+    open: true
+  }
 }
